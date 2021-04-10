a77332090c68abf08ddd66c194f32dc273a4c442
David Lapointe <info@davidweb.ca>
2021-04-10 15:29:27 -0400

    Fix for svgo 2

--- svgo.js
+++ svgo.js
@@ -12,15 +12,22 @@ function minify(data) {
   const svg = Buffer.isBuffer(data) ? data.toString() : data;
   const plugins = [];
 
-  const defaultOptions = {
-    removeTitle: false,
-    removeViewBox: false,
-  };
+  const defaultOptions = [
+    {
+      name: "removeTitle",
+      active: false
+    },
+    {
+      name: "removeViewBox",
+      active: false
+    }
+  ];
 
   // Add user plugins
   for (const plugin of Object.keys(options.plugins || [])) {
     plugins.push({
-      [plugin]: options.plugins[plugin],
+      name: options.plugins[plugin].name,
+      params: options.plugins[plugin].params
     });
   }
 
@@ -28,16 +35,19 @@ function minify(data) {
   for (const option of Object.keys(defaultOptions)) {
     if (!plugins.find(plugin => option in plugin)) {
       plugins.push({
-        [option]: defaultOptions[option],
+        name: defaultOptions[option].name,
+        params: defaultOptions[option].params
       });
     }
   }
 
-  return optimize(svg, {
+  let r = optimize(svg, {
     js2svg: {
       pretty: options.pretty,
       indent: options.indent
     },
     plugins: plugins,
-  }).then(r => Buffer.from(r.data));
+  });
+
+  return Buffer.from(r.data);
 }
